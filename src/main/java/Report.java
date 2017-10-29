b1cfb1b7ff2ed7e0d7d8bafdc62b090f64056572
TarielR <tar.iel.reg@gmail.com>
2017-10-29 15:32:07 +0300

    /

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,6 +1,7 @@
 import javax.faces.context.FacesContext;
 import javax.swing.table.DefaultTableModel;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.data.JRTableModelDataSource;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
@@ -10,12 +11,16 @@ import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 import net.sf.jasperreports.view.JasperViewer;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static java.lang.System.*;
 
 public class Report {
     private DefaultTableModel tableModel;
+    private List list = new ArrayList();
+    private final String PATH = "C:\\Java\\projects\\jasper\\";
 
     public static void main(String[] args) {
         new Report().createReport();
@@ -32,19 +37,20 @@ public class Report {
 
         try {
             JasperReport jReport = JasperCompileManager.compileReport(sourceFileName);
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(list);
             JasperPrint jPrint = JasperFillManager.fillReport(jReport, new HashMap(), new JRTableModelDataSource(report.getTableModel()));
 
 //            JasperViewer jViewer = new JasperViewer(jPrint);
 //            jViewer.setVisible(true);
 
-            JasperExportManager.exportReportToHtmlFile(jPrint, "D:\\proj\\users.html");
-            JasperExportManager.exportReportToPdfFile(jPrint, "D:\\proj\\users.pdf");
-            JasperExportManager.exportReportToXmlFile(jPrint, "D:\\proj\\users.xml", false);
+            JasperExportManager.exportReportToHtmlFile(jPrint, PATH + "users.html");
+            JasperExportManager.exportReportToPdfFile(jPrint, PATH + "users.pdf");
+            JasperExportManager.exportReportToXmlFile(jPrint, PATH + "users.xml", false);
 
             JRXlsExporter exporter = new JRXlsExporter();
 
             exporter.setExporterInput(new SimpleExporterInput(jPrint));
-            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("D:\\proj\\users.xls"));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(PATH + "users.xls"));
 
             SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
             configuration.setOnePagePerSheet(false);
